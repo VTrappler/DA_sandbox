dacd6618423ac5f07e84ab7222a81bdcef04ca6e
Victor Trappler <victor.trappler@gmail.com>
2022-01-05 17:23:19 +0100

    PF and EnKF ipynb + big refactor

---
+++ dynamicalsystems/lorenz63.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from solvers.solvers import RK4_step, integrate_step
+import matplotlib.pyplot as plt
+from dynamicalsystems.dynmodels import DynamicalModel as Model
+
+
+class Lorenz63Model(Model):
+    dim = 3
+    solver = RK4_step
+
+    @classmethod
+    def dotfunction(cls, t, x):
+        """Lorenz 96 model with constant forcing"""
+        # Setting up vector
+        d = np.zeros(3)
+        d[0] = 10 * (x[1] - x[0])
+        d[1] = 28 * x[0] - x[1] - x[0] * x[2]
+        d[2] = -x[2] * 8.0 / 3.0 + x[1] * x[0]
+        return d
+
+    @classmethod
+    def integrate(cls, t0, x0, Nsteps):
+        return integrate_step(cls.solver, cls.dotfunction, t0, x0, cls.dt, Nsteps)
+
+    @classmethod
+    def TLM(self, x):
+        return np.array(
+            [
+                [-10, 10, 0],
+                [28 - x[2], -1 - x[0]],
+                [x[1], x[0], -8.0 / 3.0],
+            ]
+        )
+
+
+def main():
+    lorenz63 = Lorenz63Model()
+    dt = 0.02
+    Lorenz63Model.dt = dt
+    lorenz63.set_initial_state(0, np.array([0, 1, 0]))
+    lorenz63.forward(5000)
+    fig = plt.figure()
+
+    for i in range(3):
+        ax = fig.add_subplot(3, 2, 2 * i + 1)
+        ax.plot(lorenz63.state_vector[i, :])
+
+    ax3d = fig.add_subplot(3, 2, (2, 4), projection="3d")
+    ax3d.scatter(*lorenz63.state_vector, c=lorenz63.t)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+# H = np.array([1, 0, 0])
+
+# def generate_obs(Ntot, burn=1000):
+#     lorenz63 = Lorenz63Model()
+#     dt = 0.02
+#     lorenz63.initial_state(np.array([0, 1, 0]))
+#     lorenz63.step(dt, Ntot)
+#     xobs = H @ lorenz63.history[1]
+#     xobs_per = xobs + np.random.normal(loc=0, scale=np.sqrt(0.2), size=(Ntot))
+#     return xobs[burn:], xobs_per[burn:]
---
+++ dynamicalsystems/lorenzIII.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+from solvers.solvers import RK4_step, integrate_step
+from dynamicalsystems.dynmodels import DynamicalModel as Model
+
+
+def phi(v, pm):
+    return np.roll(v, pm) * (np.roll(v, -pm) - np.roll(v, 2 * pm))
+
+
+def xdot(x, u, F=15, h=1, c=10, b=10):
+    return (
+        phi(x, 1)
+        + F
+        - (h * c / b) * np.fromiter(map(sum, np.split(u, len(x))), dtype="float")
+    )
+
+
+def udot(x, u, h=1, c=10, b=10):
+    return (c / b) * phi(b * u, -1) + (h * c / b) * x[np.arange(len(u)) // 10]
+
+
+def lorenz2scale_dot(t, xu):
+    c = 10  # Time scale ratio
+    b = 10  # Space scale ratio
+    h = 1  # Coupling
+    F = 15  # Forcing
+    Nx = 36
+    x, u = xu[:Nx], xu[Nx:]
+    dx = xdot(x, u, F=F, h=h, c=c, b=b)
+    du = udot(x, u, h=h, c=c, b=b)
+    return np.concatenate([dx, du])
+
+
+class Lorenz2scalesModel(Model):
+    dt = None
+    solver = RK4_step
+
+    @classmethod
+    def dotfunction(cls, t: float, xu: np.ndarray) -> np.ndarray:
+        return lorenz2scale_dot(t, xu)
+
+    @classmethod
+    def integrate(cls, t0: float, x0: np.ndarray, Nsteps: int, verbose=True):
+        return integrate_step(
+            cls.solver, cls.dotfunction, t0, x0, cls.dt, Nsteps, verbose=verbose
+        )
+
+    def __init__(self, Nx=36) -> None:
+        "docstring"
+        self.Nx = Nx
+        self.Nu = 10 * Nx
+        self.dim = self.Nx + self.Nu
+
+    @classmethod
+    def set_observation_operator(cls, H: callable) -> None:
+        cls.H = H
+
+    @classmethod
+    def set_observation_errors(cls, obserr):
+        cls.vk = obserr
+
+    def observe(self):
+        vk = self.vk()
+        return self.H(self.state_vector) + vk
+
+
+if __name__ == "__main__":
+    Nx, Nu = 36, 360
+    initial_state = np.random.normal(0, 1, size=(Nx + Nu))
+    lorenzIII = Lorenz2scalesModel()
+    dt = 0.001
+    Lorenz2scalesModel.dt = dt
+    lorenzIII.set_initial_state(0, initial_state)
+    lorenzIII.forward(10000)
+
+    lorenzIII_ = Lorenz2scalesModel()
+    initial_state[380] += 0.00001
+    lorenzIII_.set_initial_state(0, initial_state)
+    lorenzIII_.forward(10000)
+
+    plt.imshow(
+        lorenzIII.state_vector,
+        aspect="auto",
+    )
+    plt.show()
